3aacc53f222ce3105a5c73d76c7e02bf65147f90
jain_nehil <jain.nehil@gmail.com>
2018-03-24 23:34:10 -0400

    added test

--- tests/integration/test_airflow.py
+++ tests/integration/test_airflow.py
@@ -7,7 +7,7 @@ def test_airflow_dagbag():
   dagbag = DagBag(os.getenv('AIRFLOW_HOME'))
   assert dagbag.size() > 0
   report = dagbag.dagbag_report()
-  dag_id_list = ['demo_dag']
+  dag_id_list = ['demo_dag', 'db_cleanup_dag','distributaries_dag', 'source_sequential_dag', 'tributaries_dag']
   for dag_id in dag_id_list:
     assert dag_id in report
 
