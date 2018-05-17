204c4e8d55c37cd071699f6f90b243f098ffbef2
jain_nehil <jain.nehil@gmail.com>
2018-05-17 01:14:00 -0400

    refactored

--- scripts/airflow-py2-py3.py
+++ scripts/airflow-py2-py3.py
@@ -5,9 +5,7 @@ print('Python Version {}'.format(sys.version_info))
 print('Math ceil function is built in: {} and returns {}'.format(
     math.ceil, type(math.ceil(3.6))))
 
-from airflow.models import DAG
+from airflow.models import DAG  # Ignore PycodestyleBear (E402)
 
-print('Math ceil function is overridden {} and returns {}'.format(math.ceil, type(math.ceil(3.6))))
-
-
-
+print('Math ceil function is overridden {} and returns {}'.format(
+    math.ceil, type(math.ceil(3.6))))
--- tests/integration/test_airflow.py
+++ tests/integration/test_airflow.py
@@ -6,38 +6,40 @@ from airflow.models import DagBag
 
 
 def test_airflow_dagbag():
-  dagbag = DagBag(os.getenv('AIRFLOW_HOME'))
-  assert dagbag.size() > 0
-  report = dagbag.dagbag_report()
-  dag_id_list = ['demo_dag', 'db_cleanup_dag','distributaries_dag', 'source_sequential_dag', 'tributaries_dag']
-  for dag_id in dag_id_list:
-    assert dag_id in report
-
+    dagbag = DagBag(os.getenv('AIRFLOW_HOME'))
+    assert dagbag.size() > 0
+    report = dagbag.dagbag_report()
+    dag_id_list = ['demo_dag', 'db_cleanup_dag', 'distributaries_dag',
+                   'source_sequential_dag', 'tributaries_dag']
+    for dag_id in dag_id_list:
+        assert dag_id in report
 
 
 def test_dynamic_start_date():
-  """
-  Tests that start date for dags are set relative to instantiation date not a fixed date.
-  """
-  start_dates = {}
-  freezer = freeze_time('2200-02-20')
-  freezer.start()
-  dagbag = DagBag(os.getenv('AIRFLOW_HOME'))
-  for dag in dagbag.dags.values():
-    dag_start_date = dagbag.get_dag(dag.dag_id).default_args.get('start_date')
-    start_dates[dag.dag_id] = dag_start_date
-  freezer.stop()
+    """
+    Tests that start date for dags are set 
+    relative to instantiation date not a fixed date.
+    """
+    start_dates = {}
+    freezer = freeze_time('2200-02-20')
+    freezer.start()
+    dagbag = DagBag(os.getenv('AIRFLOW_HOME'))
+    for dag in dagbag.dags.values():
+        dag_start_date = dagbag.get_dag(
+            dag.dag_id).default_args.get('start_date')
+        start_dates[dag.dag_id] = dag_start_date
+    freezer.stop()
 
-  time_insensitive_dags = ['']
-  time_dependent_dags = ['']
+    time_insensitive_dags = ['']
+    time_dependent_dags = ['']
 
-
-  freezer = freeze_time('2300-02-20')
-  freezer.start()
-  dagbag = DagBag(os.getenv('AIRFLOW_HOME'))
-  for dag in dagbag.dags.values():
-    import pdb; pdb.set_trace()
-    dag_start_date = dagbag.get_dag(dag.dag_id).default_args.get('start_date')
-    assert start_dates[dag.dag_id] != dag_start_date
-  freezer.stop()
-
+    freezer = freeze_time('2300-02-20')
+    freezer.start()
+    dagbag = DagBag(os.getenv('AIRFLOW_HOME'))
+    for dag in dagbag.dags.values():
+        import pdb
+        pdb.set_trace()
+        dag_start_date = dagbag.get_dag(
+            dag.dag_id).default_args.get('start_date')
+        assert start_dates[dag.dag_id] != dag_start_date
+    freezer.stop()
